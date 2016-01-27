8b31d05bab16cd2e6292f1a9bf7218f1799eaec4
Muga Nishizawa <muga.nishizawa@gmail.com>
2016-01-27 14:49:30 -0800

    Add test methods for from_value option's long typed value

--- src/test/java/org/embulk/filter/add_time/converter/TestSchemaConverter.java
+++ src/test/java/org/embulk/filter/add_time/converter/TestSchemaConverter.java
@@ -12,6 +12,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 
+import java.util.ArrayList;
+
 import static org.embulk.filter.add_time.TestAddTimeFilterPlugin.pluginTask;
 import static org.embulk.filter.add_time.TestAddTimeFilterPlugin.schema;
 import static org.junit.Assert.assertEquals;
@@ -38,15 +40,9 @@ public class TestSchemaConverter
     {
         // if to_column doesn't exist, throws ConfigException
         {
-            ConfigSource conf = this.config.deepCopy().set("from_value", ImmutableMap.of("mode", "upload_time"));
-            Schema inputSchema = schema("c0", Types.TIMESTAMP);
-            try {
-                new SchemaConverter(log, pluginTask(conf), inputSchema);
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            ConfigSource conf = this.config.deepCopy()
+                    .set("from_value", ImmutableMap.of("mode", "upload_time"));
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
         }
 
         // if both from_column and from_value exist, throws ConfigException
@@ -55,28 +51,28 @@ public class TestSchemaConverter
                     .set("to_column", ImmutableMap.of("name", "time"))
                     .set("from_column", ImmutableMap.of("name", "c0"))
                     .set("from_value", ImmutableMap.of("mode", "upload_time"));
-            Schema inputSchema = schema("c0", Types.TIMESTAMP);
-            try {
-                new SchemaConverter(log, pluginTask(conf), inputSchema);
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
         }
 
         // if from_column or from_value doesn't exist, throws ConfigException
         {
             ConfigSource conf = this.config.deepCopy()
                     .set("to_column", ImmutableMap.of("name", "time"));
-            Schema inputSchema = schema("c0", Types.TIMESTAMP);
-            try {
-                new SchemaConverter(log, pluginTask(conf), inputSchema);
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
+        }
+
+        // if from_value type is not string or long, throws ConfigException
+        {
+            ConfigSource conf = this.config.deepCopy()
+                    .set("to_column", ImmutableMap.of("name", "time"))
+                    .set("from_value", ImmutableMap.of("mode", "fixed_time", "value", new ArrayList<Long>()));
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
+        }
+        {
+            ConfigSource conf = this.config.deepCopy()
+                    .set("to_column", ImmutableMap.of("name", "time"))
+                    .set("from_value", ImmutableMap.of("mode", "incremental_time", "from", new ArrayList<Long>(), "to", new ArrayList<Long>()));
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
         }
 
         // the type of the column specified as to_column must be 'long' or 'timestamp'
@@ -84,40 +80,19 @@ public class TestSchemaConverter
             ConfigSource conf = this.config.deepCopy()
                     .set("to_column", ImmutableMap.of("name", "time", "type", "boolean"))
                     .set("from_column", ImmutableMap.of("name", "c0"));
-            Schema inputSchema = schema("c0", Types.TIMESTAMP);
-            try {
-                new SchemaConverter(log, pluginTask(conf), inputSchema);
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
         }
         { // double
             ConfigSource conf = this.config.deepCopy()
                     .set("to_column", ImmutableMap.of("name", "time", "type", "double"))
                     .set("from_column", ImmutableMap.of("name", "c0"));
-            Schema inputSchema = schema("c0", Types.TIMESTAMP);
-            try {
-                new SchemaConverter(log, pluginTask(conf), inputSchema);
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
         }
         { // string
             ConfigSource conf = this.config.deepCopy()
                     .set("to_column", ImmutableMap.of("name", "time", "type", "string"))
                     .set("from_column", ImmutableMap.of("name", "c0"));
-            Schema inputSchema = schema("c0", Types.TIMESTAMP);
-            try {
-                new SchemaConverter(log, pluginTask(conf), inputSchema);
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            failSchemaConverterCreation(log, conf, schema("c0", Types.TIMESTAMP));
         }
 
         // the type of the column specified as from_column must be 'long', 'timestamp' or 'string'
@@ -125,20 +100,19 @@ public class TestSchemaConverter
             ConfigSource conf = this.config.deepCopy()
                     .set("to_column", ImmutableMap.of("name", "time"))
                     .set("from_column", ImmutableMap.of("name", "c0"));
-            try { // boolean
-                new SchemaConverter(log, pluginTask(conf), schema("c0", Types.BOOLEAN));
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
-            try { // double
-                new SchemaConverter(log, pluginTask(conf), schema("c0", Types.DOUBLE));
-                fail();
-            }
-            catch (Throwable t) {
-                assertTrue(t instanceof ConfigException);
-            }
+            failSchemaConverterCreation(log, conf, schema("c0", Types.BOOLEAN)); // boolean
+            failSchemaConverterCreation(log, conf, schema("c0", Types.DOUBLE)); // double
+        }
+    }
+
+    private static void failSchemaConverterCreation(Logger log, ConfigSource conf, Schema inputSchema)
+    {
+        try {
+            new SchemaConverter(log, pluginTask(conf), inputSchema);
+            fail();
+        }
+        catch (Throwable t) {
+            assertTrue(t instanceof ConfigException);
         }
     }
 
@@ -300,6 +274,17 @@ public class TestSchemaConverter
             assertEquals(inputSchema.getColumn(0), outputSchema.getColumn(0));
             assertEquals(new Column(1, "time", Types.TIMESTAMP), outputSchema.getColumn(1));
         }
+        { // specify 'unix_timestamp_unit'
+            ConfigSource conf = config.deepCopy()
+                    .set("from_value", ImmutableMap.of("mode", "fixed_time", "value", 1451643010,
+                            "unix_timestamp_unit", "sec"));
+            Schema inputSchema = schema("c0", Types.STRING);
+            Schema outputSchema = new SchemaConverter(log, pluginTask(conf), inputSchema).toOutputSchema();
+
+            assertEquals(inputSchema.size() + 1, outputSchema.size());
+            assertEquals(inputSchema.getColumn(0), outputSchema.getColumn(0));
+            assertEquals(new Column(1, "time", Types.TIMESTAMP), outputSchema.getColumn(1));
+        }
 
         // mode: incremental_time
         {
@@ -317,6 +302,17 @@ public class TestSchemaConverter
             ConfigSource conf = config.deepCopy()
                     .set("from_value", ImmutableMap.of("mode", "incremental_time",
                             "from", "2016-01-01 10:10:10.000 UTC", "to", "2016-01-01 10:10:10.000 UTC", "timestamp_format", "%Y-%m-%d %H:%M:%S.%N %Z"));
+            Schema inputSchema = schema("c0", Types.STRING);
+            Schema outputSchema = new SchemaConverter(log, pluginTask(conf), inputSchema).toOutputSchema();
+
+            assertEquals(inputSchema.size() + 1, outputSchema.size());
+            assertEquals(inputSchema.getColumn(0), outputSchema.getColumn(0));
+            assertEquals(new Column(1, "time", Types.TIMESTAMP), outputSchema.getColumn(1));
+        }
+        { // specify 'unix_timestamp_unit'
+            ConfigSource conf = config.deepCopy()
+                    .set("from_value", ImmutableMap.of("mode", "incremental_time",
+                            "from", 1451643010, "to", 1451643010, "unix_timestamp_unit", "sec"));
             Schema inputSchema = schema("c0", Types.STRING);
             Schema outputSchema = new SchemaConverter(log, pluginTask(conf), inputSchema).toOutputSchema();
 
